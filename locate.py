2315302c6973917d1aaf1e4a7b4912b50579d0c4
bourne86 <bourne86@users.noreply.github.com>
2019-06-23 13:33:46 +0100

    Update locate.py

--- locate.py
+++ locate.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Api-endpoint
-URL = "https://pos.api.here.com/positioning/v1/locate?app_id=ohiQjJyrYkY5oidVET0S&app_code=T-pIWWZu-4tqUu6aJnpywQ"
+URL = "https://pos.api.here.com/positioning/v1/locate?app_id=<app ip here>&app_code=<app code here>"
 
 # Location given here
 data = {"wlan":[{"mac": "8C-1A-BF-20-66-AD"},{"mac": "A0-E4-53-E9-66-A7"},{"mac": "AC-4B-C8-34-F7-01"},{"mac": "A0-21-95-57-79-06"},{"mac": "00-18-56-51-54-FB"},{"mac": "10-30-47-D2-54-55"},{"mac": "B8-6B-23-09-87-B1"},{"mac": "F4-55-95-11-2C-C1"}]}
